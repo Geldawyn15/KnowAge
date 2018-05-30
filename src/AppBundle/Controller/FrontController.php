deb6c39d19211365e1fc01a6e713f421c248835e
David Pellecuer <pellecuer.david@gmail.com>
2018-05-30 11:52:13 +0200

    Creation ckeditor

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -84,4 +84,14 @@ class FrontController extends controller
 
     }
 
+    /**
+     * @Route("/formation", name="HomepageFormation")
+     * @Method({"GET", "POST"})
+     */
+    public function HomepageFormationAction()
+    {
+
+        return $this->render('Front/formation.html.twig');
+    }
+
 }
