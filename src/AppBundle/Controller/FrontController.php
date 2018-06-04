e4ec857add9ffd5435493b9a123ec79cd353c29f
Romain Poilpret <romain.poilpret@gmail.com>
2018-06-04 16:20:20 +0200

    service mailer

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Service\Mailer;
 
 
 class FrontController extends controller
@@ -27,11 +29,31 @@ class FrontController extends controller
 
     /**
      * @Route("/contact", name="contact")
-     * @Method({"GET", "POST"})
+     *
      */
-    public function contactAction()
+    public function contactAction(Request $request, Mailer $mailer)
     {
-        return $this->render('Front/contact.html.twig');
+        $form = $this->createForm('AppBundle\Form\ContactType');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            $firstname = $data['firstname'];
+            $name = $data['name'];
+            $email = $data['email'];
+            $message = $data['message'];
+
+            $mailer->sendContactMail($message, $email);
+
+            return $this->redirectToRoute('search');
+     }
+
+
+        return $this->render('Front/contact.html.twig', array(
+            'form'=>$form->createView()
+        ));
     }
 
     /**
