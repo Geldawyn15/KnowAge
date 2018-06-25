db0b7644e1b72134984be7e451b0b61978eec441
Romain Poilpret <romain.poilpret@gmail.com>
2018-06-25 12:10:16 +0200

    front toujours

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Formation;
-use AppBundle\Entity\category;
+use AppBundle\Entity\Category;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
