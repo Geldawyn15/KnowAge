91f1c1cbe0493e32981a444804c53e44686506d5
Romain Poilpret <romain.poilpret@gmail.com>
2018-06-27 15:12:16 +0200

    verif form contact ok

--- src/AppBundle/Form/ContactTeacherType.php
+++ src/AppBundle/Form/ContactTeacherType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class ContactTeacherType extends AbstractType
 {
@@ -14,10 +16,21 @@ class ContactTeacherType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('objet', TextType::class)
+            ->add('email', EmailType::class, array(
+                'constraints' => array(
+                    new NotBlank()
+                )
+            ))
+            ->add('objet', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank()
+                )
+            ))
             ->add('message', TextareaType::class, array(
                 'attr' => array('cols' => '2', 'rows' => '5'),
+                'constraints' => array(
+                    new NotBlank()
+                )
             ));
     }
 }
