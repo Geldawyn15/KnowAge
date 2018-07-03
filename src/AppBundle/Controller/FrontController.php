1ea82803143f6d536663916bb9a4164e5d9fb980
David Pellecuer <pellecuer.david@gmail.com>
2018-07-03 10:29:11 +0200

    maj

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -165,11 +165,10 @@ class FrontController extends controller
     public function landingAction(Request $request, Formation $formation, Mailer $mailer)    {
 
         $entityManager = $this->getDoctrine()->getManager();
-        $shortText = $formation->shortText(250);
 
         //Affichage de la note moyenne
-        $em = $this->getDoctrine()->getManager();
-        $averageRateArray = $em->getRepository('AppBundle:Rating')->findBy(
+
+        $averageRateArray = $entityManager->getRepository('AppBundle:Rating')->findBy(
             ['formation' => $formation],
             ['rate' => 'ASC']
         );
@@ -186,71 +185,36 @@ class FrontController extends controller
         }
         //Fin d'affichage de la note moyenne
 
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $contactForm = $this->createForm(ContactTeacherType::class);
-            $contactForm->handleRequest($request);
-
-            $comment = new Comments();
-            $commentForm = $this->createForm(CommentType::class, $comment);
-            $commentForm->handleRequest($request);
-
-            $comments = $this->getDoctrine()->getRepository(Comments::class)->findBy(['formation' => $formation->getId()], ['createdAt' => 'ASC']);
-            $paginator = $this->get('knp_paginator');
-
-            $comments = $paginator->paginate(
-                $comments,
-                $request->query->getInt('page', 1),
-                9
-            );
-
-            // Post a comment
-            if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-
-                $user = $this->getUser();
-
-                $comment->setAuthor($user);
-                $comment->setCreatedAt(new \DateTime('now'));
-                $comment->setFormation($formation);
-
-                $entityManager->persist($comment);
-                $entityManager->flush();
-
-                $this->addFlash('success', 'Commentaire envoyé');
-
-                return $this->redirectToRoute('landing_formation', array(
-                    'id' => $formation->getId(),
-                ));
-            }
-
-
-            // Send mail to teacher
-            if ($contactForm->isSubmitted() && $contactForm->isValid()) {
-
-                $data = $contactForm->getData();
-                $email = $data['email'];
-                $subject = $data['objet'];
-                $message = $data['message'];
-                $to = $formation->getAuthor()->getEmail();
-
-                $mailer->sendTeacherMail('romain.poilpret@gmail.com', $message, $subject, $email);
-
-                return $this->redirectToRoute('landing_formation', array(
-                    'id' => $formation->getId(),
-                ));
-            }
-
-
-            return $this->render('Formation/landing_formation.html.twig', array(
-                'formation' => $formation,
-                'form' => $form->createView(),
-                'average' => $averagerate,
-                'contactForm' => $contactForm->createView(),
-                'commentForm' => $commentForm->createView(),
-                'shortText' => $shortText,
-                'comments' => $comments
+        $shortText = $formation->shortText(250);
+        $contactForm = $this->createForm(ContactTeacherType::class);
+        $contactForm->handleRequest($request);        $comment = new Comments();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);        $comments = $this->getDoctrine()->getRepository(Comments::class)->findBy(['formation' => $formation->getId()], ['createdAt' => 'ASC']);
+        $paginator  = $this->get('knp_paginator');        $comments = $paginator->paginate(
+            $comments,
+            $request->query->getInt('page', 1),
+            9
+        );        // Post a comment
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {            $user = $this->getUser();            $comment->setAuthor($user);
+            $comment->setCreatedAt(new \DateTime('now'));
+            $comment->setFormation($formation);            $entityManager->persist($comment);
+            $entityManager->flush();            $this->addFlash('success', 'Commentaire envoyé');            return $this->redirectToRoute('landing_formation', array(
+                'id' => $formation->getId(),
+            ));        }        // Send mail to teacher
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {            $data = $contactForm->getData();            $email = $data['email'];
+            $subject = $data['objet'];
+            $message = $data['message'];
+            $to = $formation->getAuthor()->getEmail();            $mailer->sendTeacherMail('romain.poilpret@gmail.com', $message, $subject, $email);            return $this->redirectToRoute('landing_formation', array(
+                'id' => $formation->getId(),
             ));
-        }
+        }        return $this->render('Formation/landing_formation.html.twig', array(
+            'formation' => $formation,
+            'contactForm' => $contactForm->createView(),
+            'commentForm' => $commentForm->createView(),
+            'shortText' => $shortText,
+            'comments' => $comments,
+            'average' => $averagerate
+            ));
     }
 
 
@@ -297,7 +261,7 @@ class FrontController extends controller
                 }
 
             $message = 'Vous avez attribué la note de ' .  $rate . ' / 5 à cette formation. Merci de votre contribution';
-            $this->addFlash('alert', $message);
+            $this->addFlash('success', $message);
             return $this->redirectToRoute('landing_formation', array(
                 'id' => $formation->getId(),
             ));
